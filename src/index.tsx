b17e70fca8756ec4df1e4e275b750b1065376fd0
Rajesh-shush <rchodavarapu@bexcard.co.uk>
2022-09-28 20:50:26 +0530

    #test

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,10 @@ import { EditScreen } from './Screen/editScreen';
 const SecondCustomAPP = () => {
   const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator>
+    <>
       <Stack.Screen name="secondHome" component={HomeScreen} />
       <Stack.Screen name="secondEdit" component={EditScreen} />
-    </Stack.Navigator>
+    </>
   );
 };
 

c2d0910f0c0e1348966c024d1c78be97be0a39b9
Rajesh-shush <rchodavarapu@bexcard.co.uk>
2022-09-29 14:19:40 +0530

    #comment <Test>

--- src/Screen/homeScreen.tsx
+++ src/Screen/homeScreen.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 export const HomeScreen = ({ navigation }: any) => {
+  const dispatch = useDispatch();
+  const onClick = () => {
+    dispatch({ type: 'FIRST_SCREEN' });
+    navigation.navigate('secondEdit');
+  };
   return (
     <View>
       <Text>this is home screen</Text>
       <TextInput style={styleSheet.inputContainer} />
-      <Button
-        title="GoNext"
-        onPress={() => navigation.navigate('secondEdit')}
-      />
+      <Button title="GoNext" onPress={() => onClick()} />
     </View>
   );
 };

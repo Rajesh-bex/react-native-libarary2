1615487d4b30358452d4481ce100cd559d23e0aa
Rajesh-shush <rchodavarapu@bexcard.co.uk>
2022-09-30 12:34:58 +0530

    #test

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './Screen/homeScreen';
 import { EditScreen } from './Screen/editScreen';
+import { Provider } from 'react-redux';
+import { store } from './Redux/Store';
 
 const SecondCustomAPP = () => {
   const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="secondHome" component={HomeScreen} />
-      <Stack.Screen name="secondEdit" component={EditScreen} />
-    </Stack.Navigator>
+    <Provider store={store}>
+      <Stack.Navigator>
+        <Stack.Screen name="secondHome" component={HomeScreen} />
+        <Stack.Screen name="secondEdit" component={EditScreen} />
+      </Stack.Navigator>
+    </Provider>
   );
 };
 

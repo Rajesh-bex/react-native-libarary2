e2db1ec4ba9cdb138f4a5acb6a7e9c68ba25db8b
Rajesh-shush <rchodavarapu@bexcard.co.uk>
2022-10-03 11:05:54 +0530

    #comStore

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './Screen/homeScreen';
 import { EditScreen } from './Screen/editScreen';
 import { Provider } from 'react-redux';
-import { persister, store } from './Redux/Store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './Redux/Store';
 
 const SecondCustomAPP = () => {
   const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persister}>
-        <Stack.Navigator>
-          <Stack.Screen name="secondHome" component={HomeScreen} />
-          <Stack.Screen name="secondEdit" component={EditScreen} />
-        </Stack.Navigator>
-      </PersistGate>
+      <Stack.Navigator>
+        <Stack.Screen name="secondHome" component={HomeScreen} />
+        <Stack.Screen name="secondEdit" component={EditScreen} />
+      </Stack.Navigator>
     </Provider>
   );
 };

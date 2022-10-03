4d26ef41b9052f3a228c1614721f099a8de15637
Rajesh-shush <rchodavarapu@bexcard.co.uk>
2022-10-03 10:37:13 +0530

    #commonTest

--- src/Redux/Store.tsx
+++ src/Redux/Store.tsx
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { rootReducer } from './RootReducer';
+import { persistStore } from 'redux-persist';
 
 const reducer = (state: any, action: any) => {
   return rootReducer(state, action);
@@ -16,3 +17,5 @@ export const store = createStore(
   reducer,
   compose(applyMiddleware(...middleware, logger))
 );
+
+export const persister = persistStore(store);

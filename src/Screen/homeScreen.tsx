dbc9a459b27bb553eece177b3b62f5c199f452ac
Rajesh-shush <rchodavarapu@bexcard.co.uk>
2022-11-04 15:57:33 +0530

    #test Redux

--- src/Screen/homeScreen.tsx
+++ src/Screen/homeScreen.tsx
@@ -4,20 +4,17 @@ import { useDispatch, useSelector } from 'react-redux';
 
 export const HomeScreen = ({ navigation }: any) => {
   const dispatch = useDispatch();
-  const { LibfirstScreen, commonData } = useSelector(
-    (state: any) => state.TestReducer
-  );
+  const { commonData } = useSelector((state: any) => state.TestReducer);
+  const { name } = commonData;
 
   const onClick = () => {
     dispatch({ type: 'LIB_FIRST_SCREEN' });
     navigation.navigate('secondEdit');
   };
 
-  console.log(commonData, LibfirstScreen, 'secondScreen');
-
   return (
     <View>
-      <Text>this is home screen</Text>
+      <Text>this is home screen{+' ' + name}</Text>
       <TextInput style={styleSheet.inputContainer} />
       <Button title="GoNext" onPress={() => onClick()} />
     </View>
